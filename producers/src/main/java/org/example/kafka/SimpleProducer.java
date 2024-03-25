ae9479a8a0811d653a4223691580d47fc7e08218
mike6321 <mike6321@naver.com>
2024-03-25 20:58:37 +0900

    feat : 간단한 Producer 구현

--- producers/src/main/java/org/example/kafka/SimpleProducer.java
+++ producers/src/main/java/org/example/kafka/SimpleProducer.java
@@ -1,8 +1,29 @@
 package org.example.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
 
 public class SimpleProducer {
 
-    
+    public static void main(String[] args) {
+        String topicName = "simple-topic";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, "id-001", "hello world2!");
+
+        kafkaProducer.send(producerRecord);
+
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
+
 }
